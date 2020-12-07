d466c8050a759f82d00882cc59889e91a04f8888
B-UDAY-CHANDRA-001 <budaychandra2014@gmail.com>
2020-12-07 15:07:20 +0530

    final commit

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ df = pd.read_csv('final_data(movies 1950-2020).csv')
 movies_name = list(df['movie_title'])
 df['comb'] = df['actor_1_name'] + ' ' + df['actor_2_name'] + ' '+ df['actor_3_name'] + ' '+ df['director_name'] +' ' + df['genres']
 df['comb'] = df['comb'].fillna('unknown')
+df['genres'] = df['genres'].replace('None', ' ')
 df.set_index('movie_title', inplace = True)
 
 # instantiating and generating the count matrix
@@ -44,6 +45,32 @@ def recommendations(title):
     return recommended_movies, ratings
 
 
+def genre_recomd(title):
+    recommended_movies = []
+    ratings = []
+    title = title.lower()
+    df1 = np.array(df['comb'])
+    df1 = np.append(title, df1)
+
+    idx = 0
+    count1 = CountVectorizer()
+    count_matrix = count1.fit_transform(df1)
+    c2 = cosine_similarity(count_matrix)
+
+    score_series = pd.Series(c2[0]).sort_values(ascending=False)
+
+    # getting the indexes of the 10 most similar movies
+    top_10_indexes = list(score_series.iloc[1:11].index)
+
+    # populating the list with the titles of the best 10 matching movies
+    for i in top_10_indexes:
+        recommended_movies.append(list(df.index)[i - 1])
+        ratings.append(list(df['tmdb_ratings'])[i - 1])
+
+    return recommended_movies, ratings
+
+
+
 
 app = Flask(__name__)
 
@@ -54,7 +81,17 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/genre')
+def genre():
+    return render_template('genre.html')
 
+
+@app.route("/genre_recomdf")
+def genre_recomdf():
+    title = request.args.get('title')
+    m = genre_recomd(title)[0]
+    r = genre_recomd(title)[1]
+    return render_template('recommend.html', title=title, m=m, r=r)
 
 @app.route("/recommend")
 def recommend():
@@ -62,5 +99,6 @@ def recommend():
     m = recommendations(title)[0]
     r = recommendations(title)[1]
     return render_template('recommend.html', title=title, m=m, r=r)
+
 if __name__ == '__main__':
     app.run(debug=True)
